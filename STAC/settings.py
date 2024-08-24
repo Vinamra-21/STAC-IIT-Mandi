087ae07dcdac479ec5558cc24ef352452dd6488f
Vinamra Garg <gargvinamra21@gmail.com>
2024-08-24 18:59:57 +0530

    env update

--- STAC/settings.py
+++ STAC/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Initialize environment variables
+env = environ.Env()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+# Reading the .env file
+environ.Env.read_env(os.path.join(os.path.join(BASE_DIR, '.env')))
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c7v(1uqxr5ceesm63voltrf2w9eag1xjpl*npxi&kpozx3c+e+'
+
+SECRET_KEY = env('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
