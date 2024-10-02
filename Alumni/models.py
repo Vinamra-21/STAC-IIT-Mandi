c5f35db47c0c902a289aeeb435a3b0e33e308a71
Vinamra Garg <gargvinamra21@gmail.com>
2024-10-02 18:14:32 +0530

    commit

--- Alumni/models.py
+++ Alumni/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from PIL import Image
+from ckeditor.fields import RichTextField
+import os
 
-# Create your models here.
+class Alumni(models.Model):
+    name = models.CharField(default="", max_length=50, unique=True)
+    description = RichTextField(blank=True, null=True)
+    insta = models.URLField(default="#/", blank=True, null=False)
+    linkedin = models.URLField(default="#/", blank=True, null=False)
+    image = models.ImageField(default="default.jpg", upload_to="images/Alumni")
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.image and os.path.exists(self.image.path):
+            img = Image.open(self.image.path)
+
+            output_size = (720, 1080)
+            img.thumbnail(output_size)
+
+            webp_path = os.path.splitext(self.image.path)[0] + '.webp'
+            img.save(webp_path, 'webp', optimize=True, quality=90)
+
+            self.image.name = os.path.splitext(self.image.name)[0] + '.webp'
+            super().save(*args, **kwargs)
+
--- HomePage/views.py
+++ HomePage/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
-
+from .models import Homepage, ClubActivity, Achievements, Links
 # Create your views here.
 def homepage(request):
-    return render(request, 'home.html')
+    content={
+        'homepage'=Homepage.objects.all()
+        'achievements'=Achievements.objects.all()
+        'link': Links.objects.all(),
+    }
+    return render(request, 'home.html' ,{'content':content})
+
+def home(request):
+    context_ = {
+        "club_activity": club_activity.objects.all(),
+        "competitions": homepage.objects.filter(id__in=(1, 2, 3, 4, 8)),
+        "events_intro": homepage.objects.filter(id__in=(6, 5, 7)),
+        "future_projects": homepage.objects.filter(id__in=(11, 10, 9, 12, 13, 14)),
+        "introduction": homepage.objects.get(id__in=(15,)),
+        "home": "active",
+        "achievements": achievements.objects.all(),
+        # "astrax": Links.objects.ffilter(linkname="astrax"),
+        # "pleiades": Links.objects.filter(linkname="pleiades"),
+        # "utkarsh": Links.objects.filter(linkname="utkarsh"),
+        # "zenith": Links.objects.filter(linkname="zenith"),
+        
+    }
+    return render(request, "stac_iitmandi/home.html", context_)
